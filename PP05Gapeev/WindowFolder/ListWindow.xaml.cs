b3b3b9440822730694b2de99009a82b357de79a5
egorca_05 <gapeevegor@gmail.com>
2022-06-09 23:48:45 +0300

    Подготовительные работы + база

--- PP05Gapeev/WindowFolder/ListWindow.xaml.cs
+++ PP05Gapeev/WindowFolder/ListWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PP05Gapeev
+namespace PP05Gapeev.WindowFolder
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ListWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ListWindow : Window
     {
-        public MainWindow()
+        public ListWindow()
         {
             InitializeComponent();
         }
